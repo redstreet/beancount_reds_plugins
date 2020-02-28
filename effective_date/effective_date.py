88b2b97e785031ee299cb07e0eb9326fb027e0ce
Red S <redstreet@users.noreply.github.com>
2020-02-28 02:28:50 -0800

    Add per posting effective date.

--- effective_date/effective_date.py
+++ effective_date/effective_date.py
@@ -12,6 +12,7 @@ from beancount.core import flags
 from beancount.ops import holdings
 from beancount.parser import options
 from beancount.parser import printer
+import copy
 import datetime
 import time
 import string
@@ -19,7 +20,7 @@ import random
 
 DEBUG = 0
 
-__plugins__ = ['effective_date']
+__plugins__ = ['effective_date_transaction', 'effective_date']
 
 
 # TODO:
@@ -33,11 +34,131 @@ __plugins__ = ['effective_date']
 # transaction date, the amount outstanding between the two dates likely needs
 # to be booked to an Assets:Hold account.
 
+# Bug:
+# below will fail because expense account was opened too late:
+#2014-01-01 open Expenses:Taxes:Federal
+#
+#2014-02-01 * "Estimated taxes for 2013"
+# Liabilities:Mastercard    -2000 USD
+# Expenses:Taxes:Federal  2000 USD
+#   effective_date: 2013-12-31
+
 
 LINK_FORMAT = 'effective-date-link-{}'
 
 
+# TODO:
+# Doc
+# income is held in assets
+# expenses are held in liabilities
+# - parameterize the above
+
+
+
+def has_valid_effective_date(posting):
+    return 'effective_date' in posting.meta and type(posting.meta['effective_date']) == datetime.date
+
+def has_posting_with_valid_effective_date(entry):
+    for posting in entry.postings:
+        if has_valid_effective_date(posting):
+            return True
+    return False
+
+def create_new_effective_date_entry(entry, date, hold_posting, original_posting):
+    def cleaned(p):
+       clean_meta = copy.deepcopy(p.meta)
+       clean_meta.pop('effective_date', None)
+       return p._replace(meta=clean_meta)
+
+    new_meta = {'original_date': entry.date}
+    effective_date_entry = entry._replace(date=date,
+            meta={**entry.meta, **new_meta},
+            postings=[cleaned(hold_posting), cleaned(original_posting)])
+    return effective_date_entry
+
+
 def effective_date(entries, options_map, config):
+    """Effective dates
+
+    Args:
+      entries: a list of entry instances
+      options_map: a dict of options parsed from the file
+      config: A configuration string, which is intended to be a Python dict
+        mapping match-accounts to a pair of (negative-account, position-account)
+        account names.
+    Returns:
+      A tuple of entries and errors.
+
+    """
+    start_time = time.time()
+    errors = []
+
+    interesting_entries = []
+    filtered_entries = []
+    new_accounts = set()
+    for entry in entries:
+        if isinstance(entry, data.Transaction) and has_posting_with_valid_effective_date(entry):
+            interesting_entries.append(entry)
+        else:
+            filtered_entries.append(entry)
+
+    if DEBUG:
+        print("------")
+        for e in interesting_entries:
+            printer.print_entry(e)
+        print("------")
+
+    accts_to_split = {
+            'Expenses': {'earlier': 'Liabilities:Hold:Expenses', 'later': 'Assets:Hold:Expenses'},
+            'Income':   {'earlier': 'Assets:Hold:Income', 'later': 'Liabilities:Hold:Income'},
+            }
+
+    new_entries = []
+    for entry in interesting_entries:
+        modified_entry_postings = []
+        effective_date_entry_postings = []
+        for posting in entry.postings:
+            if not has_valid_effective_date(posting):
+                modified_entry_postings += [posting]
+            else:
+                found_acct = ''
+                for acct in accts_to_split:
+                    if posting.account.startswith(acct):
+                        found_acct = acct
+
+                # find earlier or later (is this necessary?)
+                holding_account = accts_to_split[found_acct]['earlier']
+                if posting.meta['effective_date'] > entry.date:
+                    holding_account = accts_to_split[found_acct]['later']
+
+                # Replace posting in original entry with hold account
+                new_posting = posting._replace(account=posting.account.replace(found_acct, holding_account))
+                new_accounts.add(new_posting.account)
+                modified_entry_postings.append(new_posting)
+
+                # Create new entry at effective_date
+                hold_posting = new_posting._replace(units=-posting.units)
+                new_entry = create_new_effective_date_entry(entry, posting.meta['effective_date'], hold_posting, posting)
+                new_entries.append(new_entry)
+        modified_entry = entry._replace(postings=modified_entry_postings)
+        new_entries.append(modified_entry)
+
+
+    if DEBUG:
+        print("Output results:")
+        for e in new_entries:
+            printer.print_entry(e)
+
+    if DEBUG:
+        elapsed_time = time.time() - start_time
+        # print("effective_date [{:.1f}s]: {} entries inserted.".format(elapsed_time, modcount))
+
+    new_open_entries = create_open_directives(new_accounts, entries)
+    retval = new_open_entries + new_entries + filtered_entries
+    return(retval, errors)
+
+
+def effective_date_transaction(entries, options_map, config):
     """Effective dates
 
     Changes this:
