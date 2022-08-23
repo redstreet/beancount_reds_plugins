ede7830cd9f32cf3b79d44a582d8eb5858957e61
Ankur Dave <ankurdave@gmail.com>
2022-08-23 14:16:39 -0400

    rename_accounts: Copy only renamed directives for performance (#18)
    * rename_accounts: Copy only renamed directives for performance
    
    * Ignore "C901 'rename_accounts' is too complex (14)"
    
    This warning seems difficult to fix without an invasive refactoring.
    
    * Return whether rename occurred rather than using reference equality
    
    This may give us more flexibility to modify the object in place if desired.

--- beancount_reds_plugins/rename_accounts/rename_accounts.py
+++ beancount_reds_plugins/rename_accounts/rename_accounts.py
@@ -8,7 +8,7 @@ DEBUG = 0
 __plugins__ = ('rename_accounts',)
 
 
-def rename_accounts(entries, options_map, config):
+def rename_accounts(entries, options_map, config):  # noqa: C901
     """Insert entries for unmatched transactions in zero-sum accounts.
 
     Args:
@@ -30,25 +30,45 @@ def rename_accounts(entries, options_map, config):
     renames = literal_eval(config)
 
     def rename_account(account):
-        """Apply 'renames' to 'account' and return the resulting account name."""
+        """Apply 'renames' to 'account'.
+
+        Return the resulting account name and whether or not it was renamed.
+
+        """
         nonlocal rename_count
+        was_renamed = False
         for r in renames:
             if r in account:
                 account = account.replace(r, renames[r])
                 rename_count += 1
-        return account
+                was_renamed = True
+        return account, was_renamed
+
+    def rename_account_in_entry(entry, account_attr='account'):
+        """Apply 'renames' to 'getattr(entry, account_attr)'.
+
+        Return the resulting entry and whether or not it was renamed.
+
+        """
+        old_account = getattr(entry, account_attr)
+        new_account, was_renamed = rename_account(old_account)
+        new_entry = entry._replace(**{account_attr: new_account}) if was_renamed else entry
+        return new_entry, was_renamed
 
     for entry in entries:
         if isinstance(entry, data.Transaction):
             new_postings = []
+            any_posting_changed = False
             for posting in entry.postings:
-                new_postings.append(posting._replace(account=rename_account(posting.account)))
-            new_entry = entry._replace(postings=new_postings)
+                new_posting, was_renamed = rename_account_in_entry(posting)
+                any_posting_changed = any_posting_changed or was_renamed
+                new_postings.append(new_posting)
+            new_entry = entry._replace(postings=new_postings) if any_posting_changed else entry
         elif isinstance(entry, data.Pad):
-            new_entry = entry._replace(account=rename_account(entry.account),
-                                       source_account=rename_account(entry.source_account))
+            new_entry, _ = rename_account_in_entry(entry, 'account')
+            new_entry, _ = rename_account_in_entry(new_entry, 'source_account')
         elif hasattr(entry, 'account'):
-            new_entry = entry._replace(account=rename_account(entry.account))
+            new_entry, _ = rename_account_in_entry(entry)
         else:
             new_entry = entry
 
