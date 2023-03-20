085012b4b6ca71ba29235ec648957f15adce4cdb
Red S <redstreet@users.noreply.github.com>
2023-03-20 02:37:42 -0700

    refactor: and doc autoclose_tree for inclusion into Beancount

--- beancount_reds_plugins/autoclose_tree/autoclose_tree.py
+++ beancount_reds_plugins/autoclose_tree/autoclose_tree.py
@@ -1,4 +1,31 @@
-"""Closes an account subtree. See accompanying README.md"""
+"""This plugin inserts close directives for all of an account's descendants when an account is
+closed. Unopened parent accounts can also be closed. Any explicitly specified close is left
+untouched.
+
+For example, given this:
+
+```
+2017-11-10 open Assets:Brokerage:AAPL
+2017-11-10 open Assets:Brokerage:ORNG
+2018-11-10 close Assets:Brokerage  ; this account does not necessarily need to be opened
+```
+
+the plugin turns it into:
+```
+2017-11-10 open Assets:Brokerage:AAPL
+2017-11-10 open Assets:Brokerage:ORNG
+2018-11-10 close Assets:Brokerage:AAPL
+2018-11-10 close Assets:Brokerage:ORNG
+```
+
+Invoke this plugin _after_ any plugins that generate `open` directives for account trees that you
+want to auto close. An example is the `auto_accounts` plugin that ships with Beancount:
+
+```
+plugin "beancount.plugins.auto_accounts"
+plugin "beancount.plugins.close_tree"
+```
+"""
 
 import time
 from beancount.core import data
@@ -8,17 +35,12 @@ DEBUG = 0
 __plugins__ = ('autoclose_tree',)
 
 
-def autoclose_tree(entries, options_map):
+def autoclose_tree(entries, unused_options_map):
     """Insert close entries for all subaccounts of a closed account.
 
     Args:
-      entries: a list of entry instances
-
-      options_map: a dict of options parsed from the file (not used)
-
-      config: A configuration string, which is intended to be a Python dict. Currently, this is
-      ignored.
-
+      entries: A list of directives. We're interested only in the Open/Close instances.
+      unused_options_map: A parser options dict.
     Returns:
       A tuple of entries and errors. """
 
@@ -27,50 +49,23 @@ def autoclose_tree(entries, options_map):
     new_entries = []
     errors = []
 
-    opens = [e.account for e in entries if isinstance(e, Open)]
-    closes = [e.account for e in entries if isinstance(e, Close)]
+    opens = set(e.account for e in entries if isinstance(e, Open))
+    closes = set(e.account for e in entries if isinstance(e, Close))
 
     for entry in entries:
         if isinstance(entry,  Close):
-            subaccounts = [a for a in opens if entry.account + ':' in a and a not in closes]
-            for s in subaccounts:
-                meta = data.new_metadata('<beancount_reds_plugins_close_account_tree>', 0)
-                close_entry = data.Close(meta, entry.date, s)
+            subaccounts = [a for a in opens if a.startswith(entry.account + ':') and a not in closes]
+            for subacc in subaccounts:
+                meta = data.new_metadata('<beancount.plugins.close_tree>', 0)
+                close_entry = data.Close(meta, entry.date, subacc)
                 new_entries.append(close_entry)
-                closes.append(s)
-                close_count += 1
+                closes.add(subacc)  # So we don't attempt to re-close a grandchild that a child closed
             if entry.account in opens:
                 new_entries.append(entry)
         else:
             new_entries.append(entry)
 
-    # beancount.plugins.auto_accounts, if used in conjunction with this plugin, inserts an Open
-    # entry for empty parent accounts which we are trying to close. For example:
-
-    # plugin "beancount.plugins.auto_accounts"
-    # 2021-06-17 close Expenses:Non-Retirement:Auto:Fit
-    # plugin "beancount_reds_plugins.autoclose_tree.autoclose_tree"
-    # 2019-01-01 * "Transaction"
-    #   Expenses:Non-Retirement:Auto:Fit:Insurance      10 USD
-    #   Expenses:Non-Retirement:Auto:Fit:Gas            20 USD
-    #   Assets:Transfer
-
-    # Above, auto_accounts inserts an Open for 'Expenses:Non-Retirement:Auto:Fit:Gas' on 2021-06-17
-    # since it appears in a close directive. But that's not what we want. The semantic of the close
-    # when u used with this plugin is: "close all children, and close the parent if it was opened
-    # earlier. Below, we filter out entries to correctly handle this scenario:
-
-    opens = [(e.account, e.date) for e in entries if isinstance(e, Open)]
-    closes = [(e.account, e.date) for e in entries if isinstance(e, Close)]
-    retval = []
-    for entry in new_entries:
-        if isinstance(entry, Open) and (entry.account, entry.date) in closes:
-            continue
-        if isinstance(entry, Close) and (entry.account, entry.date) in opens:
-            continue
-        retval.append(entry)
-
     if DEBUG:
         elapsed_time = time.time() - start_time
         print("Close account tree [{:.2f}s]: {} close entries added.".format(elapsed_time, close_count))
-    return retval, errors
+    return new_entries, errors
