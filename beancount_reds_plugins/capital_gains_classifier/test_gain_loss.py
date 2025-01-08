068540b431f4b05c46624166aca0017ce9837f88
Stefano Mihai Canta <cantastefano@gmail.com>
2025-01-07 19:46:28 -0600

    Add test cases
    Add zero or missing price test cases. Renamed test class for gain_loss classifier

--- beancount_reds_plugins/capital_gains_classifier/test_gain_loss.py
+++ beancount_reds_plugins/capital_gains_classifier/test_gain_loss.py
@@ -15,7 +15,7 @@ config = """{
    }"""
 
 
-class TestLongShort(cmptest.TestCase):
+class TestGainLoss(cmptest.TestCase):
 
     def test_empty_entries(self):
         entries, _ = gain_loss([], options.OPTIONS_DEFAULTS.copy(), config)
@@ -66,5 +66,50 @@ class TestLongShort(cmptest.TestCase):
           Assets:Brokerage   -100 ORNG {1 USD, 2014-02-01} @ 0.50 USD
           Assets:Bank          50 USD
           Income:Capital-Gains:Losses 50 USD
+
+        """, new_entries)
+
+    @loader.load_doc()
+    def test_zero_or_missing_price(self, entries, _, options_map):
+        """
+        2014-01-01 open Assets:Brokerage
+        2014-01-01 open Assets:Bank
+        2014-01-01 open Income:Capital-Gains
+
+        2014-02-01 * "Buy"
+          Assets:Brokerage    200 ORNG {1 USD}
+          Assets:Bank        -200 USD
+
+        2017-01-01 * "Total loss for 100 ORNG"
+          Assets:Brokerage    -100 ORNG {1 USD} @ 0 USD
+          Income:Capital-Gains
+        
+        2017-01-01 * "Sell 100 ORNG, forgot price"
+          Assets:Brokerage   -100 ORNG {1 USD}
+          Assets:Bank         150 USD
+          Income:Capital-Gains
+        """
+
+        new_entries, _ = gain_loss(entries, options_map, config)
+
+        self.assertEqualEntries("""
+        2014-01-01 open Assets:Brokerage
+        2014-01-01 open Assets:Bank
+        2014-01-01 open Income:Capital-Gains
+        2014-01-01 open Income:Capital-Gains:Gains
+        2014-01-01 open Income:Capital-Gains:Losses
+
+        2014-02-01 * "Buy"
+          Assets:Brokerage    200 ORNG {1 USD}
+          Assets:Bank        -200 USD
+
+        2017-01-01 * "Total loss for 100 ORNG"
+          Assets:Brokerage    -100 ORNG {1 USD, 2014-02-01} @ 0 USD
+          Income:Capital-Gains:Losses  100 USD
+
+        2017-01-01 * "Sell 100 ORNG, forgot price"
+          Assets:Brokerage    -100 ORNG {1 USD, 2014-02-01}
+          Assets:Bank          150 USD
+          Income:Capital-Gains:Gains  -50 USD
 
         """, new_entries)
